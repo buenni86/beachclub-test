baead142d4199b8c7569a8d3eac4ad01f4380dfb
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-02-17 14:47:51 +0100

    Initial

---
+++ skript.js
@@ -0,0 +1,103 @@
+var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
+var urlTutorial = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure-erste-schritte";
+var urlFeedback = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_f26WHDvlOFNi_8Y43fECOdUMDVDTUpUUDRONkxHMzdLQ09WRlQxUUZSMS4u";
+var urlMusik = "https://www.youtube-nocookie.com/embed/36YnV9STBqc?autoplay=1";
+var urliFrame = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_f26WHDvlOFNi_8Y43fECOdURTVFV0RVVk01TE8zWTVNTTJNRE9ETkZIQi4u";
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+
+var zoneTutorial = "tutorial";
+var zoneFeedback = "feedback";
+var zoneMusik = "musik";
+var zoneiFrame = "iframe";
+
+WA.room.onEnterZone(zoneTutorial, () => {
+   currentPopup =  WA.ui.openPopup("popUpTutorial","Tutorial ansehen?",[
+        {
+            label: "OK",
+            callback: (popup => {
+                WA.nav.openTab(urlTutorial);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneTutorial, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneiFrame, () => {
+   currentPopup =  WA.ui.openPopup("popUpiFrame","Quiz starten?",[
+        {
+            label: "OK",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urliFrame);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneiFrame, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneFeedback, () => {
+   currentPopup =  WA.ui.openPopup("popUpFeedback","Hier kannst du Feedback abgeben.",[
+        {
+            label: "Feedback",
+            callback: (popup => {
+                WA.nav.openCoWebSite(urlFeedback);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneFeedback, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneMusik, () => {
+   currentPopup =  WA.ui.openPopup("popUpMusik","Ein wenig Musik?!",[
+        {
+            label: "Her damit!",
+			callback: (popup => {
+                WA.nav.openCoWebSite(urlMusik, false, "autoplay; encrypted-media");
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneMusik, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
